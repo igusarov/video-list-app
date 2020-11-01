f9654fd7297e8d1b02cfe8ea2642254ab2eba8cc
Ilya Gusarov <i.gusarov86@gmail.com>
2020-11-01 18:52:02 +0400

    fix categories selector

--- src/app/video/selectors/index.ts
+++ src/app/video/selectors/index.ts
@@ -4,7 +4,7 @@ import { createSelector } from '@ngrx/store';
 
 export const getVideos = (state: AppState): Video[] => state.video.items;
 export const getAuthors = (state: AppState): Author[] => state.author.items;
-export const getCategories = (state: AppState): Category[] => state.author.items;
+export const getCategories = (state: AppState): Category[] => state.category.items;
 
 export const getVideoById = (id: number) => createSelector<AppState, Video[], Video>(
   getVideos,
